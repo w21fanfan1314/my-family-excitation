a3b1d57d3523d1434e8af05319369eada699eca7
刘洪宝 <912393282@qq.com>
2024-12-06 01:44:36 +0800

    Update LotteryApiController.groovy

--- family-excitation-service/grails-app/controllers/family/excitation/service/api/LotteryApiController.groovy
+++ family-excitation-service/grails-app/controllers/family/excitation/service/api/LotteryApiController.groovy
@@ -8,7 +8,7 @@ import family.excitation.service.User
 class LotteryApiController {
     static allowedMethods = [saveLotteryRecords: "POST", queryLotteryRecords: "GET", giveLotteries: "GET"]
     LotteryRecordsService lotteryRecordsService
-    
+
     def saveLotteryRecords(LotteryRecords lotteryRecords) {
         if (lotteryRecords.hasErrors()) {
             respond new ApiResult(code: 500, msg: "参数错误", data: lotteryRecords.errors)
